7ff8dd7c704bf368a48eb643c82f67787fc3aae7
Casper <capper2704@hotmail.dk>
2023-09-20 12:53:49 +0200

    Made day night cyckle

--- StormsAdventure/Fighting/Enemy.cs
+++ StormsAdventure/Fighting/Enemy.cs
@@ -16,17 +16,34 @@ namespace StormsAdventure.Fighting
 
         public static void Initialize()
         {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
             if (isInitialize)
             {
                 return;
             }
+            else if (Convert.ToInt32(currentTime.TotalHours) <= 6 || Convert.ToInt32(currentTime.TotalHours) >= 18)
+            {
+                Creatures enemy1 = new Creatures("Sister", 5, 20);
+                Creatures enemy2 = new Creatures("Dog", 1, 10);
+                Creatures enemy3 = new Creatures("Cat", 10, 32);
+                Creatures enemy4 = new Creatures("Cashier", 8, 19);
+                Creatures enemy5 = new Creatures("Holigan", 7, 27);
+                enemies.Add(enemy1);
+                enemies.Add(enemy2);
+                enemies.Add(enemy3);
+                enemies.Add(enemy4);
+                enemies.Add(enemy5);
+                isInitialize = true;
+            }
+
             else
             {
                 Creatures enemy1 = new Creatures("Goblin", 5, 20);
                 Creatures enemy2 = new Creatures("Begger", 1, 10);
-                Creatures enemy3 = new Creatures("Holigan", 10, 32);
-                Creatures enemy4 = new Creatures("Cashier", 8, 19);
-                Creatures enemy5 = new Creatures("Acquaintance", 7, 27);
+                Creatures enemy3 = new Creatures("Orc", 10, 32);
+                Creatures enemy4 = new Creatures("Dragon", 8, 19);
+                Creatures enemy5 = new Creatures("Dwarf", 7, 27);
                 enemies.Add(enemy1);
                 enemies.Add(enemy2);
                 enemies.Add(enemy3);
