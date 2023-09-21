c32de717b93a95a27fa27f83fd459a0daea02c0b
Stokn9x <capper2704@hotmail.dk>
2023-09-21 22:42:14 +0200

    Storm ik copy paste, fra denne klasse. Den er kun lavet som inspiration.

---
+++ StormsAdventure/Stuff/LootTable.cs
@@ -0,0 +1,53 @@
+﻿using StormsAdventure.Character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StormsAdventure.Stuff
+{
+    public static class LootTable
+    {
+        //Storm har lavet min udgave af en lootTable, vi bruger ikke denne klasse den, skal kun bruges som inspiration, husk det nu, ikke noget med at kopiere. :)
+        private static bool isInitialize;
+
+        private static List<Items> lootTable;
+
+        public static void Initialize()
+        {
+            if (isInitialize)
+            {
+                return;
+            }
+
+            lootTable = new List<Items>();
+
+            if (!isInitialize)
+            {
+
+                Items cookie = new Items("Cookie", 10);
+                Items apple = new Items("Apple", 5);
+                Items mushroom = new Items("Mushroom", 10);
+                lootTable.Add(cookie);
+                lootTable.Add(apple);
+                lootTable.Add(mushroom);
+
+
+                isInitialize = true;
+            }
+
+
+
+        }
+
+        public static Items GetLoot()
+        {
+            Random rnd = new Random();
+
+            int num = rnd.Next(0, lootTable.Count + 1);
+
+            return lootTable[num];
+        }
+    }
+}
